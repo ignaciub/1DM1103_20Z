e777814fd4ce1a20576b28da7936890157a9387b
ignaciub <barti_293@onet.pl>
2020-11-05 16:25:17 +0100

    dawna praca wysłana w isodzie - S3.Q1 (quiz)

---
+++ sem3/S3.Q1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv[]){
+
+float x,y;
+
+scanf ("%s", &x);
+
+
+x = atof(argv[1]);
+
+
+y = x*x;
+
+printf ("Wartość %f do kwadratu to %f.\n", x, y );
+
+
+
+    return 0;
+}
